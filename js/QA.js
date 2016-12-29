f2723e8d6c7ad26292c26bc055a358ce6b9d999a
Alex <alexng3@gmail.com>
2016-12-30 01:27:38 +0800

    Add functionality to quiz

--- js/QA.js
+++ js/QA.js
@@ -10,6 +10,50 @@ var data = {
                 "d": "<javascript>"
             },
             "answer": "c"
-        }
+        },
+        {
+            "id": 2,
+            "question": "Which tag is for inserting your styling directly into the html file?",
+            "choices": {
+                "a": "<link>",
+                "b": "<css>",
+                "c": "<script>",
+                "d": "<style>"
+            },
+            "answer": "d"
+        },
+        {
+            "id": 3,
+            "question": "Which tag should contain all the metadata about your webpage?",
+            "choices": {
+                "a": "<html>",
+                "b": "<head>",
+                "c": "<body>",
+                "d": "<meta>"
+            },
+            "answer": "b"
+        },
+        {
+            "id": 4,
+            "question": "Which CSS will take priority for this code: <div class=\"foo\">bar</div>?",
+            "choices": {
+                "a": "div { color: red; }",
+                "b": ".foo { color: red; }",
+                "c": "div.foo { color: red }",
+                "d": "* { color: red; }"
+            },
+            "answer": "c"
+        },
+        {
+            "id": 5,
+            "question": "Which tag is conventionally used to display a hyperlink?",
+            "choices": {
+                "a": "<a href=\"link-to-website\">Foo</a>",
+                "b": "<link src=\"link-to-website\">Foo</link>",
+                "c": "<a src=\"link-to-website\">Foo</a>",
+                "d": "<button value=\"link-to-website\">Foo</button>"
+            },
+            "answer": "a"
+        },
     ]
 }
--- js/app.js
+++ js/app.js
@@ -1,36 +1,110 @@
 (function() {
 "use strict";
 
+var state = {
+    currentQuestion: 0,
+    currentAnswer: "",
+    correctCount: 0
+};
+
 function escapeHTML(text) {
     return text.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
 }
 
-function renderQuestions(element) {
+function goToNextQuestion(state, isFirstQuestion=false) {
+    if (!isFirstQuestion) state.currentQuestion += 1;
 
-    var questionSet = data.questions[0];
+    if (state.currentQuestion < data.questions.length) {
+        state.currentAnswer = data.questions[state.currentQuestion].answer;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function renderQuestionContent(state, element) {
+
+    var questionSet = data.questions[state.currentQuestion];
     var question = escapeHTML(questionSet.question);
-    var choiceA = escapeHTML(questionSet.choices.a);
-    var choiceB = escapeHTML(questionSet.choices.b);
-    var choiceC = escapeHTML(questionSet.choices.c);
-    var choiceD = escapeHTML(questionSet.choices.d);
+    var choices = questionSet.choices;
 
-    var result = ('<p>' + question + '</p><ul>' +
-            '<li class="a">' + choiceA + '</li>' +
-            '<li class="b">' + choiceB + '</li>' +
-            '<li class="c">' + choiceC + '</li>' +
-            '<li class="d">' + choiceD + '</li></ul>'
-    );
+    var output = '<p>' + escapeHTML(questionSet.question) + '</p><ul>';
+    for (var key in choices) {
+        output += '<li><input type="radio" name="group" value="' + key + '"> ' + escapeHTML(choices[key]) + '</input></li>';
+    }
+    output += '</ul><button class="js-submit-button">Submit Answer</button>';
 
-    element.html(result);
+    element.html(output);
+}
+
+
+function validateAnswer(state, userSubmissionElement) {
+    var choicesElement = userSubmissionElement.parent().parent();
+    var questionSet = data.questions[state.currentQuestion];
+    var answer = questionSet.answer;
+    var userResponse = userSubmissionElement.val();
+
+    choicesElement.find('input[value="' + answer + '"]').parent().addClass("correct");
+    choicesElement.siblings(".js-submit-button").attr("disabled", "disabled");
+
+    if (answer === userResponse) {
+        state.correctCount++;
+        return
+
+    } else {
+        userSubmissionElement.parent().addClass("incorrect");
+        return
+    }
+}
+
+function renderAnswerContent(state, element) {
+    var output = '<button class="js-next-button">Go To Next Question</button>';
+
+    element.html(output);
 }
 
 $(document).ready(function() {
+    var startButton = $(".js-start-button");
+    var questionContent = $(".js-question-content");
+    var answerContent = $(".js-answer-content");
 
-    $(".js-start-button").click(function(event) {
+    startButton.click(function(event) {
         event.preventDefault();
 
-        $(this).addClass("hidden");
-        renderQuestions($(".panel"));
+        $(this).parent().addClass("hidden");
+        questionContent.removeClass("hidden");
+
+        goToNextQuestion(state, true);
+        renderQuestionContent(state, questionContent);
+    });
+
+    // When submit answer clicked
+    // - check if answer is correct
+    // - show answer
+    questionContent.on("click", ".js-submit-button", function(event) {
+        event.preventDefault();
+
+        // Get user answer
+        var userSubmissionElement = $(this).siblings("ul").find("input[type='radio']:checked");
+        var result = validateAnswer(state, userSubmissionElement);
+
+        answerContent.removeClass("hidden");
+        renderAnswerContent(state, answerContent);
+    });
+
+    answerContent.on("click", ".js-next-button", function(event) {
+        event.preventDefault();
+
+        console.log(state);
+
+        answerContent.addClass("hidden");
+        var hasNextQuestion = goToNextQuestion(state);
+
+        if (hasNextQuestion) {
+            renderQuestionContent(state, questionContent);
+        } else {
+            console.log("Done!");
+        }
     });
 });
 
